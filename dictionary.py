5773fe158ed6d6430e9f94ba167f9c68e27432bb
FayRoss <mbuguafaith2@gmail.com>
2022-10-14 20:58:52 +0300

    first commit

---
+++ dictionary.py
@@ -0,0 +1,5 @@
+student={'name':'faith','reg_no':'smt/b/01-01889/2018','program':'mathematcs with It','units':['topology','complex_anlysis']}
+print(student)
+
+for message in range(5):
+    print('Welcome aboard',message,end="****")
